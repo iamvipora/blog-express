160ee12c3a386cc791b1cdbfa4e76f2a3bca7175
iamvipora <henrydizon99@gmail.com>
2024-04-22 21:06:27 +0800

    cors update

--- index.js
+++ index.js
@@ -19,11 +19,11 @@ app.use(function(req, res, next) {
   if (allowedOrigins.includes(origin)) {
        res.setHeader('Access-Control-Allow-Origin', origin);
   }
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  res.header("Access-Control-Allow-credentials", true);
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+  res.set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.set("Access-Control-Allow-credentials", true);
+  res.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
   next();
-})
+}); 
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
