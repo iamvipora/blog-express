e313ae70c5ae2d09b4ce739985bd2a4c735e4702
iamvipora <henrydizon99@gmail.com>
2024-04-22 20:45:01 +0800

    cors update

--- index.js
+++ index.js
@@ -6,12 +6,25 @@ const cors = require('cors')
 const app = express()
 const port = 3000
 
-const corsOptions = {
-  origin: `*`,
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}                   
+// const corsOptions = {
+//   origin: `*`,
+//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }                   
 
-app.use(cors(corsOptions))
+// app.use(cors(corsOptions))
+
+app.use(function(req, res, next) {
+  const allowedOrigins = ['*'];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+       res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+  next();
+});
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
