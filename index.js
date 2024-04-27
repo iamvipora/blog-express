62a332b1dfe5e687220ed5920d6f2104428803c2
iamvipora <henrydizon99@gmail.com>
2024-04-27 17:51:41 +0800

    cors update

--- index.js
+++ index.js
@@ -6,7 +6,32 @@ const cors = require('cors')
 const app = express()
 const port = 3000
 
-app.use(cors({ origin: '*'}))
+// const corsOptions = {
+//   origin: '*'
+// }
+
+// app.use(cors(corsOptions))
+
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "*"
+  )
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  )
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  )
+  res.setHeader("Access-Control-Allow-Credentials", true)
+  res.setHeader("Access-Control-Allow-Private-Network", true)
+  res.setHeader("Access-Control-Max-Age", 7200)
+
+  next()
+})
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
